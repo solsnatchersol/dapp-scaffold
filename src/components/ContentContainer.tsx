fe42cccb73469ab89b7c730c92c5d500f36a0c18
solsnatchersol <167043037+solsnatchersol@users.noreply.github.com>
2024-04-16 16:00:54 -0700

    Update ContentContainer.tsx

--- src/components/ContentContainer.tsx
+++ src/components/ContentContainer.tsx
@@ -15,28 +15,7 @@ export const ContentContainer: React.FC<Props> = ({ children }) => {
         {children}
       </div>
       {/* SideBar / Drawer */}
-      <div className="drawer-side">
-        <label htmlFor="my-drawer" className="drawer-overlay gap-6"></label>
-
-        <ul className="p-4 overflow-y-auto menu w-80 bg-base-100 gap-10 sm:flex items-center">
-          <li>
-            <Text variant="heading" className='font-extrabold tracking-tighter text-center text-transparent bg-clip-text bg-gradient-to-br from-indigo-500 to-fuchsia-500 mt-10'>Menu</Text>
-          </li>
-          <li>
-          <NavElement
-            label="Home"
-            href="/"
-          />
-          </li>
-          <li>
-          <NavElement
-            label="Basics"
-            href="/basics"
-          />
-          </li>
-        </ul>
-      </div>
+      
     </div>
   );
 };
-
