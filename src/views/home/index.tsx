b567367680b6f8806ae3f91eb0cd707c6b9e4182
solsnatchersol <167043037+solsnatchersol@users.noreply.github.com>
2024-04-16 15:07:52 -0700

    Update index.tsx

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,79 +1,31 @@
-// Next, React
-import { FC, useEffect, useState } from 'react';
-import Link from 'next/link';
-
-// Wallet
-import { useWallet, useConnection } from '@solana/wallet-adapter-react';
-
-// Components
-import { RequestAirdrop } from '../../components/RequestAirdrop';
-import { CandyMint } from '../../components/CandyMint';
-import pkg from '../../../package.json';
-
-// Store
-import useUserSOLBalanceStore from '../../stores/useUserSOLBalanceStore';
-
-export const HomeView: FC = ({ }) => {
-  const wallet = useWallet();
-  const { connection } = useConnection();
-
-  const balance = useUserSOLBalanceStore((s) => s.balance)
-  const { getUserSOLBalance } = useUserSOLBalanceStore()
-
-  useEffect(() => {
-    if (wallet.publicKey) {
-      console.log(wallet.publicKey.toBase58())
-      getUserSOLBalance(wallet.publicKey, connection)
-    }
-  }, [wallet.publicKey, connection, getUserSOLBalance])
-
-  return (
-    <>
-      <style>
-        {`
-          .custom-gradient-border {
-            background: linear-gradient(to right, #9D50BB, #6E48AA);
-            padding: 8px; // Adjusted padding
-            border-radius: 8px; // Ensuring outer container also has rounded corners
-            overflow: hidden;
-          }
-          .inner-content {
-            background: black;
-            border-radius: 8px; // Ensuring inner content also has rounded corners
-            padding: 20px; // Increased inner padding for aesthetic balance
-          }
-        `}
-      </style>
-      <div className="md:hero mx-auto p-4">
-        <div className="custom-gradient-border">
-          <div className="inner-content flex flex-col items-center">
-            <div className='mt-6 w-full flex justify-center'>
-              <img src="/feedme.png" alt="Feed Me Logo" />
-            </div>
-            <div className="flex flex-col mt-2 w-full">
-              <video
-                src="https://solsnatcher.xyz/wp-content/uploads/2024/01/SOLsnatcher.mp4#t=-1"
-                width="100%"
-                autoPlay
-                muted
-                loop
-              />
-            </div>
-            <div className="flex flex-col mt-2 w-full">
-              <RequestAirdrop />
-              <CandyMint />
-              <h4 className="text-2xl text-slate-300 mt-4 mb-2">
-                {wallet && (
-                  <div className="flex flex-row justify-center">
-                    <span className='text-slate-600'>Balance:&nbsp;</span>
-                    <span>{(balance || 0).toLocaleString()} SOL</span>
-                  </div>
-                )}
-              </h4>
-            </div>
-          </div>
+return (
+    <div className="md:hero mx-auto p-4">
+      <div className="md:hero-content flex flex-col border-2 border-white rounded-lg p-8 bg-black bg-opacity-20">
+        <div className='mt-6 w-full flex justify-center'>
+          <img src="/feedme.png" alt="Feed Me Logo" />
+        </div>
+        <div className="flex flex-col mt-2 w-full">
+          <video
+            src="https://solsnatcher.xyz/wp-content/uploads/2024/01/SOLsnatcher.mp4#t=-1"
+            width="100%"
+            autoPlay
+            muted
+            loop
+          />
+        </div>
+        <div className="flex flex-col mt-2 w-full">
+          
+          <CandyMint />
+          <h4 className="text-2xl text-slate-300 mt-4 mb-2">
+            {wallet && (
+              <div className="flex flex-row justify-center">
+                <span className='text-slate-600'>Balance:&nbsp;</span>
+                <span>{(balance || 0).toLocaleString()} SOL</span>
+              </div>
+            )}
+          </h4>
         </div>
       </div>
-    </>
+    </div>
   );
 };
