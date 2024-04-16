ed7fb0cd848c6c6f0b4deebe8a087e6ef1691201
solsnatchersol <167043037+solsnatchersol@users.noreply.github.com>
2024-04-15 22:20:25 -0700

    Update AppBar.tsx

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -20,7 +20,7 @@ export const AppBar: React.FC = () => {
       <div className="navbar flex h-20 flex-row md:mb-2 shadow-lg bg-black text-neutral-content border-b border-zinc-600 bg-opacity-66">
         <div className="navbar-start align-items-center">
           <div className="hidden sm:inline w-22 h-22 md:p-2 ml-10">
-            <img src="/solanaLogo.png"></img>
+            <img src="/sol.png"></img>
           </div>
           <WalletMultiButtonDynamic className="btn-ghost btn-sm relative flex md:hidden text-lg " />
         </div>
