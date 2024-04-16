5412503d137a7e472a57fbb21e9b3a80b375c5f7
solsnatchersol <167043037+solsnatchersol@users.noreply.github.com>
2024-04-16 15:09:35 -0700

    Update index.tsx

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,3 +1,32 @@
+// Next, React
+import { FC, useEffect, useState } from 'react';
+import Link from 'next/link';
+
+// Wallet
+import { useWallet, useConnection } from '@solana/wallet-adapter-react';
+
+// Components
+import { RequestAirdrop } from '../../components/RequestAirdrop';
+import { CandyMint } from '../../components/CandyMint';
+import pkg from '../../../package.json';
+
+// Store
+import useUserSOLBalanceStore from '../../stores/useUserSOLBalanceStore';
+
+export const HomeView: FC = ({ }) => {
+  const wallet = useWallet();
+  const { connection } = useConnection();
+
+  const balance = useUserSOLBalanceStore((s) => s.balance)
+  const { getUserSOLBalance } = useUserSOLBalanceStore()
+
+  useEffect(() => {
+    if (wallet.publicKey) {
+      console.log(wallet.publicKey.toBase58())
+      getUserSOLBalance(wallet.publicKey, connection)
+    }
+  }, [wallet.publicKey, connection, getUserSOLBalance])
+
 return (
     <div className="md:hero mx-auto p-4">
       <div className="md:hero-content flex flex-col border-2 border-white rounded-lg p-8 bg-black bg-opacity-20">
