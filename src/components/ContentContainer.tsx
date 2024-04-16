261e64734a59df7b52ee17f3146f6d163d65aafb
solsnatchersol <167043037+solsnatchersol@users.noreply.github.com>
2024-04-16 16:04:04 -0700

    Update ContentContainer.tsx

--- src/components/ContentContainer.tsx
+++ src/components/ContentContainer.tsx
@@ -10,11 +10,11 @@ export const ContentContainer: React.FC<Props> = ({ children }) => {
 
   return (
     <div className="flex-1 drawer h-52 flex-col justify-between">
-      <input id="my-drawer" type="checkbox" className="grow drawer-toggle" />
+      
       <div className="items-center drawer-content flex flex-col justify-between">
-        {children}
+        
       </div>
-      {/* SideBar / Drawer */}
+      
       
     </div>
   );
