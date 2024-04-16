1c4324daad97e9f22ff769e90c4bf20596aadc2c
solsnatchersol <167043037+solsnatchersol@users.noreply.github.com>
2024-04-15 20:32:39 -0700

    Update CandyMint.tsx

--- src/components/CandyMint.tsx
+++ src/components/CandyMint.tsx
@@ -1,57 +1,35 @@
-import { useConnection, useWallet } from '@solana/wallet-adapter-react';
-import { LAMPORTS_PER_SOL, TransactionSignature } from '@solana/web3.js';
-import { FC, useCallback } from 'react';
-import { notify } from "../utils/notifications";
-import useUserSOLBalanceStore from '../stores/useUserSOLBalanceStore';
+//...Default Imports (we will replace these later)
 
-export const RequestAirdrop: FC = () => {
+export const CandyMint: FC = () => {
+    // 👇 Update these constant declarations
     const { connection } = useConnection();
-    const { publicKey } = useWallet();
+    const wallet = useWallet();
     const { getUserSOLBalance } = useUserSOLBalanceStore();
 
+    // TODO - Create an Umi instance
+
+    // 👇 Update this onClick function
     const onClick = useCallback(async () => {
-        if (!publicKey) {
-            console.log('error', 'Wallet not connected!');
-            notify({ type: 'error', message: 'error', description: 'Wallet not connected!' });
-            return;
-        }
-
-        let signature: TransactionSignature = '';
-
-        try {
-            signature = await connection.requestAirdrop(publicKey, LAMPORTS_PER_SOL);
-
-            // Get the lates block hash to use on our transaction and confirmation
-            let latestBlockhash = await connection.getLatestBlockhash()
-            await connection.confirmTransaction({ signature, ...latestBlockhash }, 'confirmed');
-
-            notify({ type: 'success', message: 'Airdrop successful!', txid: signature });
-
-            getUserSOLBalance(publicKey, connection);
-        } catch (error: any) {
-            notify({ type: 'error', message: `Airdrop failed!`, description: error?.message, txid: signature });
-            console.log('error', `Airdrop failed! ${error?.message}`, signature);
-        }
-    }, [publicKey, connection, getUserSOLBalance]);
+      if (!publicKey) {
+          console.log('error', 'Wallet not connected!');
+          notify({ type: 'error', message: 'error', description: 'Wallet not connected!' });
+          return;
+      }
+      // TODO - Add minting logic here
+    }, []);
 
     return (
-
         <div className="flex flex-row justify-center">
-                <div className="relative group items-center">
-                    <div className="m-1 absolute -inset-0.5 bg-gradient-to-r from-indigo-500 to-fuchsia-500 
+            <div className="relative group items-center">
+                <div className="m-1 absolute -inset-0.5 bg-gradient-to-r from-indigo-500 to-fuchsia-500 
                     rounded-lg blur opacity-20 group-hover:opacity-100 transition duration-1000 group-hover:duration-200 animate-tilt"></div>
-            
-                        <button
-                            className="px-8 m-2 btn animate-pulse bg-gradient-to-br from-indigo-500 to-fuchsia-500 hover:from-white hover:to-purple-300 text-black"
-                            onClick={onClick}
-                            >
-                                <span>Airdrop 1 </span>
-                
-                        </button>
-                </div>
+                <button
+                    className="px-8 m-2 btn animate-pulse bg-gradient-to-br from-indigo-500 to-fuchsia-500 hover:from-white hover:to-purple-300 text-black"
+                    onClick={onClick}
+                >
+                    <span>Mint NFT </span>
+                </button>
+            </div>
         </div>
-
-        
     );
 };
-
